8a96c17e8d6e01051b5da14a70cdf2e22d20df0c
Sayee Deshmukh <sayyeee21@gmail.com>
2025-04-20 16:27:16 +0530

    Created login and logout

--- store/urls.py
+++ store/urls.py
@@ -2,4 +2,6 @@ from .views import RegistrationView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 class RegistrationView(View):
     def get(self, request):
@@ -18,3 +19,24 @@ class RegistrationView(View):
 
         User.objects.create_user(username=username, password=password)
         return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'store/login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'store/login.html', {'error': 'Invalid credentials'}) 
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')           
