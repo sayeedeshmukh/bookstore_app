35e1c5b9aeae88742a2b226364443f085d7d6999
Sayee Deshmukh <sayyeee21@gmail.com>
2025-04-20 10:17:51 +0530

    created html files templates

--- bookstore/accounts/views.py
+++ bookstore/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
-# Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'registration/register.html')
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        
+        if password != password2:
+            messages.error(request, "Passwords don't match!")
+            return redirect('register')
+        
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect('book-list')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'registration/login.html')
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('book-list')
+        else:
+            messages.error(request, "Wrong username or password!")
+            return redirect('login')
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
